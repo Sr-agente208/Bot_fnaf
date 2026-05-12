f1b4ce229bd7a598e3a023ee812f262fdcd8dbb0
Sr-agente208 <sr.agente208@gmail.com>
2026-05-12 14:49:24 -0300

    Add FNAF bot functionality with Baileys integration

--- index.js
+++ index.js
@@ -1,4 +1,59 @@
-// Bot_fnaf - Main entry point
-// Created on 2026-05-12
+const {
+default: makeWASocket,
+useMultiFileAuthState,
+DisconnectReason
+} = require('@whiskeysockets/baileys')
 
-console.log('Bot FNAF initialized');
+async function startBot() {
+
+const { state, saveCreds } =
+await useMultiFileAuthState('auth')
+
+const sock = makeWASocket({
+auth: state
+})
+
+sock.ev.on('creds.update', saveCreds)
+
+sock.ev.on('messages.upsert', async ({ messages }) => {
+
+const m = messages[0]
+
+if (!m.message) return
+
+const from = m.key.remoteJid
+
+const body =
+m.message.conversation || ''
+
+if (body === '!fnaf') {
+
+const frases = [
+'👁️ Freddy está te observando...',
+'🦊 Foxy saiu da Pirate Cove!',
+'🔋 Sua energia está acabando...'
+]
+
+const imagens = [
+'https://i.imgur.com/WxNkK7J.jpeg',
+'https://i.imgur.com/6XQJv8x.jpeg'
+]
+
+const resultado =
+frases[Math.floor(Math.random() * frases.length)]
+
+const imagem =
+imagens[Math.floor(Math.random() * imagens.length)]
+
+await sock.sendMessage(from, {
+image: { url: imagem },
+caption: resultado
+})
+
+}
+
+})
+
+}
+
+startBot()
